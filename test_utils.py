bedcc02827b4c0dda8c38f46894cbe60270bb838
Puck573 <experemen7@gmail.com>
2026-03-25 09:54:30 +0800

    Break test to demonstrate CI failure

--- test_utils.py
+++ test_utils.py
@@ -3,7 +3,7 @@ import utils
 
 def test_is_even():
     assert utils.is_even(2) == True
-    assert utils.is_even(3) == False
+    assert utils.is_even(3) == True  # Ошибка: 3 не должно быть чётным
     assert utils.is_even(0) == True
 
 def test_generate_password():
@@ -11,4 +11,4 @@ def test_generate_password():
     assert len(password) == 10
     
     password2 = utils.generate_password(10)
-    assert password != password2  # С большой вероятностью пароли разные
+    assert password != password2
